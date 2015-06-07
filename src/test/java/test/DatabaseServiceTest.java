313c4fbb7ad7119bce4391eeb4c8e165b38bce0c
Leonardo Cappuccio <leo@systemexception.org>
2015-06-07 22:58:53 +0200

    green: testGetTimeStamp

--- src/test/java/test/DatabaseServiceTest.java
+++ src/test/java/test/DatabaseServiceTest.java
@@ -2,7 +2,9 @@ package test;
 
 import org.junit.Test;
 import org.systemexception.h2embedded.api.Database;
+import org.systemexception.h2embedded.api.Logger;
 import org.systemexception.h2embedded.impl.DatabaseService;
+import org.systemexception.h2embedded.impl.LoggerImpl;
 
 import java.sql.SQLException;
 
@@ -14,15 +16,23 @@ import static org.junit.Assert.assertTrue;
  */
 public class DatabaseServiceTest {
 
-	private Database database = new DatabaseService();
+    private static final Logger logger = LoggerImpl.getFor(DatabaseService.class);
+	private Database sut = new DatabaseService();
 
 	@Test
 	public void testIsRunning() throws Exception {
-		assertTrue(database.isRunning());
+		assertTrue(sut.isRunning());
 	}
 
 	@Test
 	public void testShutdown() throws SQLException {
-		assertTrue(database.shutdown());
+		assertTrue(sut.shutdown());
 	}
+
+    @Test
+    public void testGetTimeStamp() throws SQLException {
+        String currentTimeStamp = sut.getCurrentTimeStamp();
+        logger.info(currentTimeStamp);
+        assert (currentTimeStamp != null);
+    }
 }
