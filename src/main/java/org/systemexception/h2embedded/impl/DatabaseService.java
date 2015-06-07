ff84b5e7dd6ccd1e4f060119a5499f945db21a56
Leonardo Cappuccio <leo@systemexception.org>
2015-06-07 21:56:54 +0200

    refactor

--- src/main/java/org/systemexception/h2embedded/impl/DatabaseService.java
+++ src/main/java/org/systemexception/h2embedded/impl/DatabaseService.java
@@ -2,6 +2,7 @@ package org.systemexception.h2embedded.impl;
 
 import org.systemexception.h2embedded.api.Database;
 import org.systemexception.h2embedded.api.Logger;
+import org.systemexception.h2embedded.enums.DatabaseProperties;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,9 +21,9 @@ public class DatabaseService implements Database {
 		try
 		{
 			Class.forName("org.h2.Driver");
-			String url = "jdbc:h2:mem";
-			String user = "h2";
-			String password = "h2";
+			String url = DatabaseProperties.DATABASE_URL.toString();
+			String user = DatabaseProperties.DATABASE_USER.toString();
+			String password = DatabaseProperties.DATABASE_PASSWORD.toString();
 			connection = DriverManager.getConnection(url, user, password);
 			logger.info("Starting database");
 		}
