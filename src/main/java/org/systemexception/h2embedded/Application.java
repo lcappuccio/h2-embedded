7cf67509726a3545d98ac2502f2af2fad1953347
Leonardo Cappuccio <leo@systemexception.org>
2015-10-11 16:26:24 +0200

    add h2 web console

--- src/main/java/org/systemexception/h2embedded/Application.java
+++ src/main/java/org/systemexception/h2embedded/Application.java
@@ -1,8 +1,11 @@
 package org.systemexception.h2embedded;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +21,13 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
 	}
 
 }
