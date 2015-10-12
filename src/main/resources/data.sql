98c2da191eaac4a4e053fe04be00b3329ae3e1f0
Leonardo Cappuccio <leo@systemexception.org>
2015-10-12 11:56:39 +0200

    add data initialize script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- Initialize data
+insert into data values (1,'TestData1');
+insert into data values (2,'TestData2');
+insert into data values (3,'TestData3');
+insert into data values (4,'TestData4');
+insert into data values (5,'TestData5');
+insert into data values (6,'TestData6');
+insert into data values (7,'TestData7');
+insert into data values (8,'TestData8');
+insert into data values (9,'TestData9');
+insert into data values (10,'TestData10');
