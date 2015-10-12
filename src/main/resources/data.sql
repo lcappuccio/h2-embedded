4ca8db4983063d4c1320feab5e448759b8d79a85
Leonardo Cappuccio <leo@systemexception.org>
2015-10-12 13:04:41 +0200

    add timestamp attribute

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 -- Initialize data
-insert into data values (1,'TestData1');
-insert into data values (2,'TestData2');
-insert into data values (3,'TestData3');
-insert into data values (4,'TestData4');
-insert into data values (5,'TestData5');
-insert into data values (6,'TestData6');
-insert into data values (7,'TestData7');
-insert into data values (8,'TestData8');
-insert into data values (9,'TestData9');
-insert into data values (10,'TestData10');
+insert into data (data_id, data_value, data_timestamp) values (1,'TestData1', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (2,'TestData2', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (3,'TestData3', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (4,'TestData4', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (5,'TestData5', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (6,'TestData6', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (7,'TestData7', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (8,'TestData8', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (9,'TestData9', current_timestamp());
+insert into data (data_id, data_value, data_timestamp) values (10,'TestData10', current_timestamp());
