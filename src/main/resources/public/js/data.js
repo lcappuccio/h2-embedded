edb62ffba4814c4054e91b98974341b026375c2e
Leonardo Cappuccio <leo@systemexception.org>
2015-11-01 22:42:04 +0100

    code style

--- src/main/resources/public/js/data.js
+++ src/main/resources/public/js/data.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-	$.get("http://localhost:8080/api/data", function(data) {
+$(document).ready(function () {
+	$.get("http://localhost:8080/api/data", function (data) {
 		console.log("data loaded");
 		$('.data-size').append(data.length);
 		drawTable(data);
@@ -8,16 +8,16 @@ $(document).ready(function() {
 
 function drawTable(data) {
 	// console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        drawRow(data[i]);
-    }
+	for (var i = 0; i < data.length; i++) {
+		drawRow(data[i]);
+	}
 }
 
 function drawRow(rowData) {
-    var row = $("<tr/>")
-    $("#dataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
-    row.append($("<td>" + rowData.dataId + "</td>"));
-    row.append($("<td>" + rowData.dataValue + "</td>"));
+	var row = $("<tr/>")
+	$("#dataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+	row.append($("<td>" + rowData.dataId + "</td>"));
+	row.append($("<td>" + rowData.dataValue + "</td>"));
 	var date = new Date(rowData.dataTimestamp).toLocaleString();
-    row.append($("<td>" + date + "</td>"));
+	row.append($("<td>" + date + "</td>"));
 }
