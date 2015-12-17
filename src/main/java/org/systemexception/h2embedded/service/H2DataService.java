e29abd980e2efa521160d33bf9c8fd47f376d6bf
Leonardo Cappuccio <leo@systemexception.org>
2015-12-17 22:48:59 +0100

    fallback to default logger

--- src/main/java/org/systemexception/h2embedded/service/H2DataService.java
+++ src/main/java/org/systemexception/h2embedded/service/H2DataService.java
@@ -1,13 +1,13 @@
 package org.systemexception.h2embedded.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.systemexception.h2embedded.domain.Data;
 import org.systemexception.h2embedded.repositories.DataRepository;
-import org.systemexception.logger.api.Logger;
-import org.systemexception.logger.impl.LoggerImpl;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -23,7 +23,7 @@ import java.util.List;
 @Transactional
 public class H2DataService implements DataService {
 
-	private static final Logger logger = LoggerImpl.getFor(H2DataService.class);
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	private final DataRepository dataRepository;
 
 	@Autowired
