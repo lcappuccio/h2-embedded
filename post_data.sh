1f7f7c872d35e41c30bf8dfef3f744a6f4b62c79
Leonardo Cappuccio <leo@systemexception.org>
2015-12-27 17:27:25 +0100

    use correct bash syntax

--- post_data.sh
+++ post_data.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash 
 COUNTER=0
-while [  $COUNTER -lt 100 ]; do
+while [  ${COUNTER} -lt 100 ]; do
 	data=`openssl rand -base64 32`
-    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data '{"dataValue":"'$data'"}' http://xxx.xxx.xxx.xxx:8080/api/data
+    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data '{"dataValue":"'${data}'"}' http://localhost:8080/api/data
     let COUNTER=COUNTER+1 
 done
 
