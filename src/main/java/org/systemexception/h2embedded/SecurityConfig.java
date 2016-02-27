28e880cadad31cdf82e429aea7be07ad19803069
Leonardo Cappuccio <leo@systemexception.org>
2016-02-27 13:29:55 +0100

    setup security

--- src/main/java/org/systemexception/h2embedded/SecurityConfig.java
+++ src/main/java/org/systemexception/h2embedded/SecurityConfig.java
@@ -1,25 +1,36 @@
 package org.systemexception.h2embedded;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.systemexception.h2embedded.constants.Endpoints;
 
 /**
  * @author leo
  * @date 27/02/16 11:31
  */
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final String ADMIN_ROLE = "ADMIN", USER_ROLE = "USER";
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("ADMIN").password("ADMIN").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("admin").password("admin_pwd").roles(ADMIN_ROLE);
+		auth.inMemoryAuthentication().withUser("user").password("user_pwd").roles(USER_ROLE);
 	}
 
 	@Override
 	public void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().anyRequest().authenticated();
+		httpSecurity.authorizeRequests().antMatchers(Endpoints.H2_CONSOLE).hasRole(ADMIN_ROLE).and().httpBasic();
+		httpSecurity.authorizeRequests().antMatchers(HttpMethod.DELETE).hasRole(ADMIN_ROLE).and().httpBasic();
+		httpSecurity.authorizeRequests().antMatchers(HttpMethod.POST).hasRole(ADMIN_ROLE).and().httpBasic();
+		httpSecurity.authorizeRequests().antMatchers(HttpMethod.PUT).hasRole(ADMIN_ROLE).and().httpBasic();
+		httpSecurity.csrf().disable().headers().frameOptions().sameOrigin();
 	}
 }
