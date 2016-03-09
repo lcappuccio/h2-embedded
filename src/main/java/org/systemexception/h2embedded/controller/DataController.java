d1ce1396a84bfc75e547a2825b3d88f3f592c656
Leonardo Cappuccio <cappuccio@strands.com>
2016-03-09 11:29:33 +0100

    replace with empty <>

--- src/main/java/org/systemexception/h2embedded/controller/DataController.java
+++ src/main/java/org/systemexception/h2embedded/controller/DataController.java
@@ -1,22 +1,30 @@
 package org.systemexception.h2embedded.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.systemexception.h2embedded.constants.Endpoints;
 import org.systemexception.h2embedded.constants.Parameters;
 import org.systemexception.h2embedded.domain.Data;
 import org.systemexception.h2embedded.service.DataService;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 import javax.validation.Valid;
-import java.util.List;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author leo
@@ -43,7 +51,7 @@ public class DataController {
 	@ResponseBody
 	public ResponseEntity<Data> create(@RequestBody @Valid Data data) {
 		logger.info("Received CREATE: " + data.getDataValue());
-		return new ResponseEntity<Data>(dataService.create(data), HttpStatus.CREATED);
+		return new ResponseEntity<>(dataService.create(data), HttpStatus.CREATED);
 	}
 
 	@ApiOperation(value = "Delete data", notes = "Deletes data from the database")
@@ -66,9 +74,9 @@ public class DataController {
 		logger.info("Received Get: " + id);
 		Data dataById = dataService.findById(Integer.valueOf(id));
 		if(dataById != null) {
-			return new ResponseEntity<Data>(dataById, HttpStatus.FOUND);
+			return new ResponseEntity<>(dataById, HttpStatus.FOUND);
 		} else {
-			return new ResponseEntity<Data>(dataById, HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(dataById, HttpStatus.NOT_FOUND);
 		}
 
 	}
@@ -79,7 +87,7 @@ public class DataController {
 	@ResponseBody
 	public ResponseEntity<List<Data>> findAll() {
 		logger.info("Received GET all persons");
-		return new ResponseEntity<List<Data>>(dataService.findAll(), HttpStatus.OK);
+		return new ResponseEntity<>(dataService.findAll(), HttpStatus.OK);
 	}
 
 	@ApiOperation(value = "Update data", notes = "Unknown behaviour if id does not exist")
@@ -88,9 +96,9 @@ public class DataController {
 	public ResponseEntity<Data> update(@RequestBody @Valid Data data) {
 		logger.info("Received UPDATE: " + data.getDataId() + ", " + data.getDataValue());
 		if (dataService.update(data)) {
-			return new ResponseEntity<Data>(dataService.findById(data.getDataId()), HttpStatus.OK);
+			return new ResponseEntity<>(dataService.findById(data.getDataId()), HttpStatus.OK);
 		} else {
-			return new ResponseEntity<Data>(new Data(), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(new Data(), HttpStatus.NOT_FOUND);
 		}
 	}
 }
