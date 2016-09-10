0383c57427d428adc007912ddf2afe8b4880760c
Leonardo Cappuccio <leo@systemexception.org>
2016-09-10 15:15:21 +0200

    restore security, extract static string

--- src/test/java/org/systemexception/h2embedded/test/DataControllerTest.java
+++ src/test/java/org/systemexception/h2embedded/test/DataControllerTest.java
@@ -1,7 +1,6 @@
 package org.systemexception.h2embedded.test;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.systemexception.h2embedded.Application;
+import org.systemexception.h2embedded.SecurityConfig;
 import org.systemexception.h2embedded.controller.DataController;
 import org.systemexception.h2embedded.domain.Data;
 import org.systemexception.h2embedded.service.DataService;
@@ -28,6 +28,8 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.systemexception.h2embedded.constants.Endpoints.CONTEXT;
+import static org.systemexception.h2embedded.constants.Endpoints.PATH_SEPARATOR;
 
 /**
  * @author leo
@@ -40,8 +42,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class DataControllerTest {
 
 	private final Data data = new Data();
-	public final static String TEST_DATA = "TestData";
-	private final static String ENDPOINT = "/api/data/";
+	final static String TEST_DATA = "TestData";
 	private final Long existingId = 1L, nonExistingId = 99L;
 	@Autowired
 	private FilterChainProxy springSecurityFilterChain;
@@ -69,7 +70,7 @@ public class DataControllerTest {
 
 	@Test
 	public void find_all_persons() throws Exception {
-		sut.perform(MockMvcRequestBuilders.get(ENDPOINT).accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status()
+		sut.perform(MockMvcRequestBuilders.get(CONTEXT).accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status()
 				.is(HttpStatus.OK.value()));
 		verify(dataService).findAll();
 	}
@@ -77,90 +78,92 @@ public class DataControllerTest {
 	@Test
 	public void find_single_data() throws Exception {
 		dataService.create(data);
-		sut.perform(MockMvcRequestBuilders.get(ENDPOINT + data.getDataId()).accept(MediaType.APPLICATION_JSON_VALUE))
-				.andExpect(status().is(HttpStatus.FOUND.value()));
+		sut.perform(MockMvcRequestBuilders.get(CONTEXT + PATH_SEPARATOR + data.getDataId())
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().is(HttpStatus.FOUND.value()));
 		verify(dataService).findById(existingId);
 	}
 
 	@Test
 	public void dont_find_non_existing_data() throws Exception {
-		sut.perform(MockMvcRequestBuilders.get(ENDPOINT + nonExistingId).accept(MediaType.APPLICATION_JSON_VALUE))
-				.andExpect(status().is(HttpStatus.NOT_FOUND.value()));
+		sut.perform(MockMvcRequestBuilders.get(CONTEXT + PATH_SEPARATOR + nonExistingId)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().is(HttpStatus.NOT_FOUND.value()));
 		verify(dataService).findById(nonExistingId);
 	}
 
 	@Test
-	@WithMockUser(username = "admin", password = "admin_pwd", roles = {"ADMIN"})
+	@WithMockUser(username = SecurityConfig.ADMIN_USER, password = SecurityConfig.ADMIN_PASSWORD,
+			roles = {SecurityConfig.ADMIN_ROLE})
 	public void save_data() throws Exception {
-		sut.perform(MockMvcRequestBuilders.post(ENDPOINT).contentType(MediaType.APPLICATION_JSON_VALUE).content
+		sut.perform(MockMvcRequestBuilders.post(CONTEXT).contentType(MediaType.APPLICATION_JSON_VALUE).content
 				(dataJson(data).getBytes())).andExpect(status().is(HttpStatus.CREATED.value()));
 		verify(dataService).create(any());
 	}
 
-	// TODO LC Re-enable with security
 	@Test
-	@Ignore
-	@WithMockUser(username = "user", password = "user_pwd", roles = {"USER"})
+	@WithMockUser(username = SecurityConfig.USER_USER, password = SecurityConfig.USER_PASSWORD,
+			roles = {SecurityConfig.USER_ROLE})
 	public void save_data_forbidden() throws Exception {
-		sut.perform(MockMvcRequestBuilders.post(ENDPOINT).contentType(MediaType.APPLICATION_JSON_VALUE).content
+		sut.perform(MockMvcRequestBuilders.post(CONTEXT).contentType(MediaType.APPLICATION_JSON_VALUE).content
 				(dataJson(data).getBytes())).andExpect(status().is(HttpStatus.FORBIDDEN.value()));
 		verifyNoMoreInteractions(dataService);
 	}
 
 	@Test
-	@WithMockUser(username = "admin", password = "admin_pwd", roles = {"ADMIN"})
+	@WithMockUser(username = SecurityConfig.ADMIN_USER, password = SecurityConfig.ADMIN_PASSWORD,
+			roles = {SecurityConfig.ADMIN_ROLE})
 	public void delete_existing_data() throws Exception {
 		when(dataService.update(data)).thenReturn(true);
-		sut.perform(MockMvcRequestBuilders.delete(ENDPOINT + "/" + existingId)).andExpect(status()
+		sut.perform(MockMvcRequestBuilders.delete(CONTEXT + PATH_SEPARATOR + existingId)).andExpect(status()
 				.is(HttpStatus.OK.value()));
 		verify(dataService).delete(existingId);
 	}
 
 	@Test
-	@WithMockUser(username = "admin", password = "admin_pwd", roles = {"ADMIN"})
+	@WithMockUser(username = SecurityConfig.ADMIN_USER, password = SecurityConfig.ADMIN_PASSWORD,
+			roles = {SecurityConfig.ADMIN_ROLE})
 	public void delete_nonexisting_data() throws Exception {
 		when(dataService.update(data)).thenReturn(false);
-		sut.perform(MockMvcRequestBuilders.delete(ENDPOINT + "/" + nonExistingId)).andExpect(status()
+		sut.perform(MockMvcRequestBuilders.delete(CONTEXT + PATH_SEPARATOR + nonExistingId)).andExpect(status()
 				.is(HttpStatus.NOT_FOUND.value()));
 		verify(dataService).delete(nonExistingId);
 	}
 
-	// TODO LC Re-enable with security
 	@Test
-	@Ignore
-	@WithMockUser(username = "user", password = "user_pwd", roles = {"USER"})
+	@WithMockUser(username = SecurityConfig.USER_USER, password = SecurityConfig.USER_PASSWORD,
+			roles = {SecurityConfig.USER_ROLE})
 	public void delete_forbidden() throws Exception {
 		when(dataService.update(data)).thenReturn(false);
-		sut.perform(MockMvcRequestBuilders.delete(ENDPOINT + "/" + nonExistingId)).andExpect(status()
+		sut.perform(MockMvcRequestBuilders.delete(CONTEXT + PATH_SEPARATOR + nonExistingId)).andExpect(status()
 				.is(HttpStatus.FORBIDDEN.value()));
 		verifyNoMoreInteractions(dataService);
 	}
 
 	@Test
-	@WithMockUser(username = "admin", password = "admin_pwd", roles = {"ADMIN"})
+	@WithMockUser(username = SecurityConfig.ADMIN_USER, password = SecurityConfig.ADMIN_PASSWORD,
+			roles = {SecurityConfig.ADMIN_ROLE})
 	public void update_existing_data() throws Exception {
 		when(dataService.update(any())).thenReturn(true);
-		sut.perform(MockMvcRequestBuilders.put(ENDPOINT).contentType(MediaType.APPLICATION_JSON_VALUE).content
+		sut.perform(MockMvcRequestBuilders.put(CONTEXT).contentType(MediaType.APPLICATION_JSON_VALUE).content
 				(dataJson(data).getBytes())).andExpect(status().is(HttpStatus.OK.value()));
 		verify(dataService).update(any());
 	}
 
 	@Test
-	@WithMockUser(username = "admin", password = "admin_pwd", roles = {"ADMIN"})
+	@WithMockUser(username = SecurityConfig.ADMIN_USER, password = SecurityConfig.ADMIN_PASSWORD,
+			roles = {SecurityConfig.ADMIN_ROLE})
 	public void update_nonexisting_data() throws Exception {
 		when(dataService.update(any())).thenReturn(false);
-		sut.perform(MockMvcRequestBuilders.put(ENDPOINT).contentType(MediaType.APPLICATION_JSON_VALUE).content
+		sut.perform(MockMvcRequestBuilders.put(CONTEXT).contentType(MediaType.APPLICATION_JSON_VALUE).content
 				(dataJson(data).getBytes())).andExpect(status().is(HttpStatus.NOT_FOUND.value()));
 		verify(dataService).update(any());
 	}
 
-	// TODO LC Re-enable with security
 	@Test
-	@Ignore
-	@WithMockUser(username = "user", password = "user_pwd", roles = {"USER"})
+	@WithMockUser(username = SecurityConfig.USER_USER, password = SecurityConfig.USER_PASSWORD,
+			roles = {SecurityConfig.USER_ROLE})
 	public void update_forbidden() throws Exception {
 		when(dataService.update(any())).thenReturn(false);
-		sut.perform(MockMvcRequestBuilders.put(ENDPOINT).contentType(MediaType.APPLICATION_JSON_VALUE).content
+		sut.perform(MockMvcRequestBuilders.put(CONTEXT).contentType(MediaType.APPLICATION_JSON_VALUE).content
 				(dataJson(data).getBytes())).andExpect(status().is(HttpStatus.FORBIDDEN.value()));
 		verifyNoMoreInteractions(dataService);
 	}
